47e1bca72c9e84e66098da9c02016a8750695827
evelinepurnomo <lilian95@rocketmail.com>
2015-10-29 23:09:35 +0700

    Update UTS.sh

--- UTS.sh
+++ UTS.sh
@@ -1,2 +1,5 @@
-wget -nd -r -l 1 -P \input -A jpg -e robots=off http://www.itb.ac.id
+#Using rsync using shell scripting
+#Reference: http://stackoverflow.com/questions/4602153/how-do-i-use-wget-to-download-all-images-into-a-single-folder
+
+wget -nd -r -l 1 -P /input -A jpg -e robots=off http://www.itb.ac.id
 rsync -a /input /backup
