9228098710de4d2cc8d01e6088ed15ac822b6015
evelinepurnomo <lilian95@rocketmail.com>
2015-10-29 23:27:06 +0700

    Update UTS.sh

--- UTS.sh
+++ UTS.sh
@@ -1,4 +1,4 @@
-#Using rsync using shell scripting
+#Using rsync in shell scripting
 #Reference: http://stackoverflow.com/questions/4602153/how-do-i-use-wget-to-download-all-images-into-a-single-folder
 
 wget -nd -r -l 1 -P /input -A jpg -e robots=off http://www.itb.ac.id
