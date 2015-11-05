d346e06bb2afa65b33a6a05150f86b8bda3ff2fa
evelinepurnomo <lilian95@rocketmail.com>
2015-11-05 12:15:46 +0700

    Update rest-api.php

--- rest-api.php
+++ rest-api.php
@@ -1,20 +1,20 @@
 <?php
-	function get_info_by_id() {
+	function get_info_by_id($id) {
 		$info = array();
-		mysql_connect('localhost', 'root',' ');
-		mysql_select_db('coba);
-		$info = mysql_query('SELECT * FROM ini nama table WHERE id=$id');
+		mysql_connect('localhost','root','');
+		mysql_select_db('shop1');
+		$result = mysql_query('SELECT id, member_name, join_year FROM shop_member WHERE id = ' . $id);
+		$info = mysql_fetch_array ($result, MYSQL_ASSOC);
 		return $info;
-	
-		if (isset ($_GET["action"])) {
-			switch ($_GET["action"]) {
-				case "get_info";
-					if (isset($_GET["id"]))
-						$value = get_info_by_id(["id"]);
-					else
-						$value = "ERROR";
-					break;
-			}
+	}
+	if (isset($_GET["action"])) {
+		switch ($_GET["action"]) {
+			case "get_info" ;
+				if (isset($_GET["id"]))
+					$value = get_info_by_id ($_GET["id"]);
+				else
+					$value = "ERROR";
+				break;
 		}
 	}
 	exit (json_encode($value));
